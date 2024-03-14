d3f090949f5c1492b1cb8f928a2bb8c3a28e2dc9
abdulrahmanalaa123 <abdulrahmanalaa497@gmail.com>
2024-03-14 19:02:34 +0200

    testing auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,64 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Banners from "./components/Banners";
+import { supabase } from "../../lib/supabaseClient";
+import userStore from "../../stores/user";
+import { useShallow } from "zustand/react/shallow";
 
 export default function Home() {
+  const setUserData = userStore((state) => state.setUserData);
+  async function testFunction() {
+    // const { data: signUpData, error: signUpError } =
+    //   await supabase.auth.signInWithPassword({
+    //     email: "abdulrahmanalaa497@gmail.com",
+    //     password: "3brazk2023",
+    //   });
+    const { data: signUpData, error: signUpError } =
+      await supabase.auth.signInWithPassword({
+        email: "example@email.com",
+        password: "example-password",
+      });
+
+    // if (signUpError) {
+    //   console.error(signUpError);
+    // } else {
+    //   console.log(signUpData);
+    // }
+
+    // const { data, error } = await supabase.auth.signUp({
+    //   email: "example@email.com",
+    //   password: "example-password",
+    //   options: {
+    //     data: {
+    //       first_name: "John",
+    //       last_name: "Doe",
+    //       address: "mahmodl",
+    //       imageUrl: "https://picsum.photos/600",
+    //     },
+    //   },
+    // });
+
+    // const {
+    //   data: { user },
+    // } = await supabase.auth.getUser();
+    console.log(signUpData["user"]["user_metadata"]);
+    setUserData(signUpData["user"]["user_metadata"]);
+    console.log(localStorage.getItem("token"));
+    console.log(userData);
+    // await supabase.auth.signOut();
+    // deleteUserData();
+  }
   return (
     <>
       <Hero />
+      <button
+        className="rounded-full px-4 py-1 bg-slate-400"
+        onClick={() => {
+          testFunction();
+        }}
+      >
+        Testing
+      </button>
       <Banners />
     </>
   );
